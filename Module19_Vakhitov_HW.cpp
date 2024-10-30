3143c4b86b59ec93da4947a86c6afe1f2b878a9e
AthaCro <lordoftimuria@yandex.ru>
2024-10-30 21:46:55 +0300

    Add project files.

---
+++ Module19_Vakhitov_HW.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+	virtual void Voice() {}
+	virtual ~Animal(){}
+};
+
+class dog : public Animal
+{
+	void Voice() override
+	{
+		cout << "Dogge says: woof!" << endl;
+	}
+};
+
+class cat : public Animal
+{
+	void Voice() override
+	{
+		cout << "Cat says: meow!" << endl;
+	}
+};
+
+class wolf : public Animal
+{
+	void Voice() override
+	{
+		cout << "Wolf says: AUF!" << endl;
+	}
+};
+
+int main()
+{
+	const int size = 3;
+	Animal* Animals[size] = { new dog, new cat, new wolf };
+	for (int i = 0; i < size; i++)
+	{
+		Animals[i]->Voice();
+		delete Animals[i];
+	}
+	return 0;
+}
+
